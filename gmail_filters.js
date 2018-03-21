102faae2120fe2e31efdc9d344679ee9e7ddcddd
William Falk-Wallace <fww@google.com>
2018-03-20 21:03:06 -0400

    adds spacing and fixes quotes

--- gmail_filters.js
+++ gmail_filters.js
@@ -1,53 +1,51 @@
-const xmlHeader = (name, email, updated) => `
-<?xml version='1.0' encoding='utf-8' ?>
-<feed xmlns:apps='http://schemas.google.com/apps/2006' xmlns='http://www.w3.org/2005/Atom'>
-<title>Mail Filters</title>
-<id>tag:mail.google.com,2008:filters:</id>
-<updated>${date}</updated>
-<author>
-  <name>${name}</name>
-  <email>${email}</email>
-</author>
+const xmlHeader = (name, email, updated) => `<?xml version="1.0" encoding="utf-8" ?>
+<feed xmlns:apps="http://schemas.google.com/apps/2006" xmlns="http://www.w3.org/2005/Atom">
+    <title>Mail Filters</title>
+    <id>tag:mail.google.com,2008:filters:</id>
+    <updated>${updated}</updated>
+    <author>
+        <name>${name}</name>
+        <email>${email}</email>
+    </author>
 `;
 
 const xmlFooter = "</feed>";
 
-const entry = filterContent => `
-<entry><category term="filter"></category>
-  <title>Mail Filter</title>
-  <content></content>
-  ${filterContent}
-</entry>
+const entry = filterContent => `    <entry>
+        <category term="filter"></category>
+        <title>Mail Filter</title>
+        <content></content>
+${filterContent}
+    </entry>
 `;
 
-const getLabelXml = label => "<apps:property name='label' value='${label}'></apps:property>";
-const getNeverSpamXml = _ => "<apps:property name='shouldNeverSpam' value='true'></apps:property>";
-const getMarkReadXml = _ => "<apps:property name='shouldMarkAsRead' value='true'></apps:property>";
-const getShouldArchiveXml = _ => "<apps:property name='shouldArchive' value='true'></apps:property>";
-const getArchiveIndirectXml = me => `<apps:property name="doesNotHaveTheWord" value='${me}'></apps:property>`;
-
+const getLabelXml = label => '        <apps:property name="label" value="${label}"></apps:property>';
+const getNeverSpamXml = _ => '        <apps:property name="shouldNeverSpam" value="true"></apps:property>';
+const getMarkReadXml = _ => '        <apps:property name="shouldMarkAsRead" value="true"></apps:property>';
+const getShouldArchiveXml = _ => '        <apps:property name="shouldArchive" value="true"></apps:property>';
+const getArchiveIndirectXml = me => `        <apps:property name="doesNotHaveTheWord" value="${me}"></apps:property>`;
+const getHasXml = value => `        <apps:property name="hasTheWord" value="${value}"></apps:property>`;
 
 const getHasPart = hasValue => {
   if (typeof hasValue === 'string') {
     return hasValue;
   } else if (hasValue.and) {
     debugger;
-    return `(${hasValue.and.map(getHasPart).join(" AND ")})`;
+    return `(${hasValue.and.map(getHasPart).join(' AND ')})`;
   } else if (hasValue.or) {
     debugger;
-    return `(${hasValue.or.map(getHasPart).join(" OR ")})`;
+    return `(${hasValue.or.map(getHasPart).join(' OR ')})`;
   }
 };
-const getHasXml = value => `<apps:property name='hasTheWord' value='${value}'></apps:property>`;
 const getHasRule = hasValue => getHasXml(getHasPart(hasValue));
 
 const convert = (json) => {
   const date = new Date().toISOString();
-  const name = "William Falk-Wallace";
-  const email = "wfalkwallace@gmail.com";
+  const name = 'William Falk-Wallace';
+  const email = 'wfalkwallace@gmail.com';
   let xml = [xmlHeader(name, email, date)];
 
-  const me = `(${json.me.join(" OR ")})`;
+  const me = `(${json.me.join(' OR ')})`;
   const filters = json.filters;
   const filterXml = filters.reduce((xml, filter) => {
     let filterContent = [];
